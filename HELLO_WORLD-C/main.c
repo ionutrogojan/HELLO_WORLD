8219e37766d97fea874625812267db96ad4d82c4
Ionut Rogojan <34418533+ionutrogojan@users.noreply.github.com>
2021-12-07 01:19:23 +0000

    Update main.c

--- HELLO_WORLD-C/main.c
+++ HELLO_WORLD-C/main.c
@@ -4,7 +4,7 @@
 
 int main() {
     char greeting[16] = {'H', 'e', 'l', 'l', 'o', ' ', 'W', 'o', 'r', 'l', 'd', ',', ' ', 'C', '.'};
-    // char greeting1[] = "Hello World 1. C.";
+    // char greeting1[] = "Hello World 1, C.";
     printf("%s", greeting);
     return 0;
 }
