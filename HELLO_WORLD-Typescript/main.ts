57d75bd201230b8505ea91ee6810a84b80d45ff3
Ionut Rogojan <34418533+ionutrogojan@users.noreply.github.com>
2023-08-12 23:28:37 +0100

    Update main.ts in Typescript
    Updated "JavaScript" to "TypeScript"

--- HELLO_WORLD-Typescript/main.ts
+++ HELLO_WORLD-Typescript/main.ts
@@ -1,6 +1,6 @@
 // Hello World Typescript
 
-const greeting: string = "Hello World, JavaScript.";
+const greeting: string = "Hello World, TypeScript.";
 console.log(greeting);
 
 // playground: https://www.typescriptlang.org/play
